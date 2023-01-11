7b54284f8b4532c063b0ce661c92faf559149bb2
tanvirss13 <79608956+tanvirss13@users.noreply.github.com>
2023-01-11 01:44:44 -0500

    Update assignment.cpp

--- Introduction_to_Concurrent_Programming/Week_1/assignment.cpp
+++ Introduction_to_Concurrent_Programming/Week_1/assignment.cpp
@@ -1,1 +1,52 @@
+#include <string.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <sstream>
+#include "core_functions.hpp"
+using namespace std;
 
+const string USERNAME = "Tanvir Singh Sethi";
+
+int main(int argc, char *argv[])
+{
+  int results = 1;
+  string username = "";
+
+  ifstream fin(".user");
+  if (!fin)
+  {
+    cout << "Error opening file. Shutting down..." << endl;
+    return 1;
+  }
+
+  char *usernameFromFile;
+  // get current line
+  for (std::string line; getline(fin, line, '\n');)
+  {
+    usernameFromFile = &line[0];
+  }
+
+  fin.close();
+  setenv("username", usernameFromFile, 0);
+  if (argc > 1)
+  {
+    username = argv[1];
+  }
+
+  bool validUser = verifyUser(username);
+  setenv("username", &USERNAME[0], 0);
+  validUser = validUser && verifyUser(username);
+
+  if (validUser)
+  {
+    printMessage(username);
+    results = 0;
+  }
+  else
+  {
+    cout << "Error your usernames don't match check code and .user file." << endl;
+  }
+  return results;
+}
